6549f7d4f7e525a4e7f053ba11a517f56e323831
Andreas Pelme <andreas@pelme.se>
2008-11-02 20:23:08 +0100

    Break up AJAXSimpleExceptionResponse to middleware.py instead of http.py

--- peritus/http.py
+++ peritus/http.py
@@ -1,23 +1,8 @@
-from django.conf import settings
-from django.http import HttpResponseServerError, HttpResponseBadRequest, HttpResponse
+from django.http import HttpResponse
 from django.utils import simplejson
 from django.core.serializers import serialize
 from django.db.models.query import QuerySet
 
-# This snippet was stolen from
-# http://www.djangosnippets.org/snippets/650/
-class AJAXSimpleExceptionResponse:
-    def process_exception(self, request, exception):
-        if settings.DEBUG and request.is_ajax():
-            import sys, traceback
-            exc_type, exc_info, tb = sys.exc_info()
-            response = "%s\n" % exc_type.__name__
-            response += "%s\n\n" % exc_info
-            response += "TRACEBACK:\n"
-            for tb in traceback.format_tb(tb):
-                response += "%s\n" % tb
-
-            return HttpResponseServerError(response)
 
 # This snippet was stolen from
 # http://www.djangosnippets.org/snippets/154/
