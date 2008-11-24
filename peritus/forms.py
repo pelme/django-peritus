efb322b550d5f550245c17acf7c827933c23465e
Andreas Pelme <andreas@pelme.se>
2008-11-24 11:51:17 +0100

    Added a workaround for validating ModelForms with unique fields. Snippet from http://www.djangosnippets.org/snippets/649/

--- peritus/forms.py
+++ peritus/forms.py
@@ -12,3 +12,44 @@ class AsUlFix(object):
             ...
         """
         return self._html_output(u'<li>%(errors)s%(label)s %(field)s<p>%(help_text)s</p></li>', u'<li>%s</li>', '</li>', u' %s', False)
+
+from django import forms
+
+class CleanUniqueField:
+    """Wrap the clean_XXX method."""
+
+    def __init__(self, form, name):
+        try:
+            self.clean = getattr(form, 'clean_' + name)
+        except AttributeError:
+            self.clean = None
+
+        self.form = form
+        self.name = name
+
+    def __call__(self):
+        if self.clean:
+            result = self.clean()
+        else:
+            result = self.form.cleaned_data[self.name]
+
+        queryset = self.form._meta.model.objects
+        if self.form.instance.pk:
+            queryset = queryset.exclude(pk=self.form.instance.pk)
+
+        try:
+            queryset.get(**{self.name: result})
+        except self.form._meta.model.DoesNotExist:
+            return result
+        else:
+            raise forms.ValidationError(_(u'The %s is already in use.') % self.verbose_name)
+
+class ModelForm(forms.ModelForm):
+    """A hack around the Django ticket #4895."""
+
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
+
+        for field in self._meta.model._meta.fields:
+            if field.unique and field.name in self.base_fields:
+                setattr(self, 'clean_' + field.name, CleanUniqueField(self, field.name))
