7aca63bfb4b365e73b24133f9d8781962ea86d6b
Andreas Pelme <andreas@pelme.se>
2009-01-02 16:18:35 +0100

    Fix emailbackend to work with recent versions of django.

--- peritus/emailbackend.py
+++ peritus/emailbackend.py
@@ -1,7 +1,16 @@
 # http://www.djangosnippets.org/snippets/74/
+import re
 
 from django.contrib.auth.models import User
-from django.core.validators import email_re
+
+
+email_re = re.compile(
+    r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*" \
+    r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"' \
+    r')@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$', re.IGNORECASE)
+
+
+
 
 class BasicBackend:
     def get_user(self, user_id):
@@ -24,6 +33,7 @@ class EmailBackend(BasicBackend):
                 user = User.objects.get(username=username)
             except User.DoesNotExist:
                 return None
+
         if user.check_password(password):
             return user
 
