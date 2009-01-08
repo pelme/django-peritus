49cc6d4d1667d9c3f4c25374ad00060200bee65f
Andreas Pelme <andreas@pelme.se>
2009-01-08 17:06:24 +0100

    Add context processor for setting context variable for the "stupid admin" which does not use a proper url resolver.

--- peritus/context_processors.py
+++ peritus/context_processors.py
@@ -2,3 +2,6 @@ from django.conf import settings as django_settings
 
 def settings(request):
     return {'settings': django_settings,}
+    
+def dumb_admin(request):
+    return {'root_path': '/admin/'}
