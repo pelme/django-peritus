2c677f5b58f4ed2845e8b9ab092570f27bc03582
Andreas Pelme <andreas@pelme.se>
2009-03-01 17:47:51 +0100

    fieldsets

---
+++ peritus/templatetags/fieldsets.py
@@ -0,0 +1,35 @@
+import copy
+
+from django import template
+from django import forms
+from django.utils.datastructures import SortedDict
+
+register = template.Library()
+
+
+# {% get_fieldset <fields> as foo from form %}
+
+def get_fieldset(parser, token):
+    try:
+        name, fields, as_, variable_name, from_, form = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError('badly formatted arguments for %r'  % token.split_contents()[0])
+
+    return FieldSetNode([f.strip() for f in fields.split(',')], variable_name, form)
+
+get_fieldset = register.tag(get_fieldset)
+
+
+class FieldSetNode(template.Node):
+    def __init__(self, fields, variable_name, form_variable):
+        self.fields = fields
+        self.variable_name = variable_name
+        self.form_variable = form_variable
+
+    def render(self, context):
+        form = template.Variable(self.form_variable).resolve(context)
+        new_form = copy.copy(form)
+        new_form.fields = SortedDict([(key, value) for key, value in form.fields.items() if key in self.fields])
+        context[self.variable_name] = new_form
+
+        return u''
