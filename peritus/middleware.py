a0ffb361b6c5d72c78f87efc937594894f44ca2e
Andreas Pelme <andreas@pelme.se>
2012-03-08 17:59:28 +0100

    Make 404 AJAX calls nicer

--- peritus/middleware.py
+++ peritus/middleware.py
@@ -1,15 +1,22 @@
+import sys
+import traceback
+
 from django.conf import settings
-from django.http import HttpResponseServerError
+from django.http import HttpResponseServerError, Http404, HttpResponseNotFound
 
 # This snippet was stolen from
 # http://www.djangosnippets.org/snippets/650/
 # Add this line to MIDDLEWARE_CLASSES:
 # 'peritus.middleware.AJAXSimpleExceptionResponse',
 
+
 class AJAXSimpleExceptionResponse:
     def process_exception(self, request, exception):
         if settings.DEBUG and request.is_ajax():
-            import sys, traceback
+            # Special case for Http404 exceptions
+            if isinstance(exception, Http404):
+                return HttpResponseNotFound(exception.message)
+
             exc_type, exc_info, tb = sys.exc_info()
             response = "%s\n" % exc_type.__name__
             response += "%s\n\n" % exc_info
@@ -18,4 +25,3 @@ class AJAXSimpleExceptionResponse:
                 response += "%s\n" % tb
 
             return HttpResponseServerError(response)
-
