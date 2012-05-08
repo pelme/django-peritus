0be94aa1b4e56afebe07deb9c10ae5e8fd2adf28
Andreas Pelme <andreas@pelme.se>
2012-05-08 22:27:33 +0200

    Order fields by specified order, not by original field order

--- peritus/templatetags/fieldsets.py
+++ peritus/templatetags/fieldsets.py
@@ -1,7 +1,6 @@
 import copy
 
 from django import template
-from django import forms
 from django.utils.datastructures import SortedDict
 
 register = template.Library()
@@ -13,7 +12,7 @@ def get_fieldset(parser, token):
     try:
         name, fields, as_, variable_name, from_, form = token.split_contents()
     except ValueError:
-        raise template.TemplateSyntaxError('badly formatted arguments for %r'  % token.split_contents()[0])
+        raise template.TemplateSyntaxError('badly formatted arguments for %r' % token.split_contents()[0])
 
     return FieldSetNode([f.strip() for f in fields.split(',')], variable_name, form)
 
@@ -29,7 +28,7 @@ class FieldSetNode(template.Node):
     def render(self, context):
         form = template.Variable(self.form_variable).resolve(context)
         new_form = copy.copy(form)
-        new_form.fields = SortedDict([(key, value) for key, value in form.fields.items() if key in self.fields])
+        new_form.fields = SortedDict([(field_name, form.fields[field_name]) for field_name in self.fields])
         context[self.variable_name] = new_form
 
         return u''
